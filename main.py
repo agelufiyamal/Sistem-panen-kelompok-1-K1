f95cc0730435a05403234b331ab611cbebd02ab9
agelufiyamal <agelufiyamal@gmail.com>
2026-09-21 22:43:34 +0700

    Menyelesaikan merge conflict

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from diskon import hitung_diskon
 
 def main():
     print("================================")
-<<<<<<< HEAD
-    print("Sistem Pencatat Hasil Panen - Malah")
-=======
-    print("Sistem Pencatat Hasil Panen - Tivani")
->>>>>>> conflict-anggota-b
+    print("Sistem Pencatat Hasil Panen - Malah dan Tivani")
     print("================================")
 
     data_panen = input_data_panen()
